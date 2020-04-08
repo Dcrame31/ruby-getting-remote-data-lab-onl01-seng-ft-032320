74dd407ef2e0032cf93b5ac3a2d277163959405c
Diane Crame <dcrame@icloud.com>
2020-04-08 22:56:16 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,10 +13,13 @@ class GetRequester
   end
   
   def get_response
-    
+    uri = URI.parse(@string_url)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
 
   def parse_json
+    JSON.parse(response.body)
   end
 
 end
